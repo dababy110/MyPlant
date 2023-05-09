bfc4267bb9a5f6133de12d35eb72c4bc09126131
david <89177980+DbfanBra@users.noreply.github.com>
2023-05-08 21:58:45 -0600

    Motor-Movement-GUI

--- src/components/MyPlant.jsx
+++ src/components/MyPlant.jsx
@@ -6,9 +6,11 @@ import {
   Button,
   Image,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 
 import { Ionicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 import { initializeApp } from "firebase/app";
 import { getDatabase, onValue, ref, set } from "firebase/database";
@@ -30,15 +32,14 @@ const SlideUpView = Animatable.createAnimatableComponent(View);
 
 const app = initializeApp(firebaseConfig);
 
-db = getDatabase(app);
-
-
 const MyPlant = () => {
   const [ledStatus, setLedStatus] = useState(null);
   const [analogStatus, setAnalogStatus] = useState("");
   const [showComponent, setShowComponent] = useState(false);
+  const [isCheckMotorMovement, setCheckMotorMovement] = useState(null);
 
   useEffect(() => {
+    const db = getDatabase(app);
     const analogStatusRef = ref(db, "analogStatus");
     onValue(analogStatusRef, (snapshot) => {
       const data = snapshot.val();
@@ -46,13 +47,25 @@ const MyPlant = () => {
     });
   }, []);
 
+  const handleToggle = () => {
+    const db = getDatabase(app);
+    const CheckMotorMovement = ref(db, "MotorMovement/CheckMotorMovement");
+    set(CheckMotorMovement, false);
+    setCheckMotorMovement(!isCheckMotorMovement);
+    if (!isCheckMotorMovement) {
+      set(CheckMotorMovement, true);
+    }
+  };
+
   const handleTurnOnLed = () => {
+    const db = getDatabase(app);
     const ledStatusRef = ref(db, "ledStatus");
     set(ledStatusRef, 1);
     setLedStatus("on");
   };
 
   const handleTurnOffLed = () => {
+    const db = getDatabase(app);
     const ledStatusRef = ref(db, "ledStatus");
     set(ledStatusRef, 0);
     setLedStatus("off");
@@ -62,93 +75,247 @@ const MyPlant = () => {
     setShowComponent(!showComponent);
   };
 
+
+  const MotorMovementUpIn = () => {
+    const db = getDatabase(app);
+    const MotorMovementRef = ref(db, "MotorMovement/MotorMovement/Up");
+    set(MotorMovementRef, 1);
+  };
+
+  const MotorMovementUpOut = () => {
+    const db = getDatabase(app);
+    const MotorMovementRef = ref(db, "MotorMovement/MotorMovement/Up");
+    set(MotorMovementRef, 0);
+  };
+
+  const MotorMovementRightIn = () => {
+    const db = getDatabase(app);
+    const MotorMovementRef = ref(db, "MotorMovement/MotorMovement/Right");
+    set(MotorMovementRef, 1);
+  };
+
+  const MotorMovementRightOut = () => {
+    const db = getDatabase(app);
+    const MotorMovementRef = ref(db, "MotorMovement/MotorMovement/Right");
+    set(MotorMovementRef, 0);
+  };
+
+  const MotorMovementLeftIn = () => {
+    const db = getDatabase(app);
+    const MotorMovementRef = ref(db, "MotorMovement/MotorMovement/Left");
+    set(MotorMovementRef, 1);
+  };
+
+  const MotorMovementLeftOut = () => {
+    const db = getDatabase(app);
+    const MotorMovementRef = ref(db, "MotorMovement/MotorMovement/Left");
+    set(MotorMovementRef, 0);
+  };
+
+  const MotorMovementDownIn = () => {
+    const db = getDatabase(app);
+    const MotorMovementRef = ref(db, "MotorMovement/MotorMovement/Down");
+    set(MotorMovementRef, 1);
+  };
+
+  const MotorMovementDownOut = () => {
+    const db = getDatabase(app);
+    const MotorMovementRef = ref(db, "MotorMovement/MotorMovement/Down");
+    set(MotorMovementRef, 0);
+  };
+
+
   return (
-    <View style={styles.container}>
-      <View style={styles.card}>
-        <Text style={styles.title}>Status</Text>
-        <Text>Estado del led: {ledStatus}</Text>
-        <View style={styles.containerButtons}>
-          <Button onPress={handleTurnOnLed} title="Encender LED" />
-          <Button onPress={handleTurnOffLed} title="Apagar LED" />
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.card}>
+          <Text style={styles.title}>Status</Text>
+          <Text>Estad del led: {ledStatus}</Text>
+          <View style={styles.containerButtons}>
+            <Button onPress={handleTurnOnLed} title="Encender LED" />
+            <Button onPress={handleTurnOffLed} title="Apagar LED" />
+          </View>
         </View>
-      </View>
-      <View style={styles.card}>
-        <Text style={styles.title}>AnalogStatus</Text>
-        <Text>Medicion con el potenciometro: </Text>
-        <Text style={{ marginTop: 20, fontSize: 45 }}>{analogStatus}</Text>
-      </View>
-      <View style={styles.section}>
-        <TouchableOpacity>
-          <Text style={styles.textSection}>Planta de Tomate</Text>
-          <Image
-            style={styles.imgStack}
-            source={require("../../assets/img/imagen-2.png")}
-          />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.section}>
-        <TouchableOpacity onPress={handleComponent}>
-          <Text style={styles.textSection}>Planta de Aguacate</Text>
-          <Image
-            style={styles.imgStack}
-            source={require("../../assets/img/imagen-2.png")}
-          />
-        </TouchableOpacity>
-      </View>
-      {/* <View style={styles.test}>
-          <Text>aa</Text>
-        </View> */}
-      {showComponent && (
-        <SlideUpView
-          animation="slideInUp"
+        <View style={styles.card}>
+          <Text style={styles.title}>AnalogStatus</Text>
+          <Text>Medicion con el potenciometro: </Text>
+          <Text style={{ marginTop: 20, fontSize: 45 }}>{analogStatus}</Text>
+        </View>
+        <View style={styles.section}>
+          <TouchableOpacity>
+            <Text style={styles.textSection}>Planta de Tomate</Text>
+            <Image
+              style={styles.imgStack}
+              source={require("../../assets/img/imagen-2.png")}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.section}>
+          <TouchableOpacity onPress={handleComponent}>
+            <Text style={styles.textSection}>Planta de Aguacate</Text>
+            <Image
+              style={styles.imgStack}
+              source={require("../../assets/img/imagen-2.png")}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View
           style={{
-            backgroundColor: "#fff",
-            height: "100%",
-            width: 410,
-            position: "absolute",
-            marginLeft: -50,
-            marginTop: 100,
+            marginTop: 10,
             padding: 30,
-            borderRadius: 20,
+            flexDirection: "colum",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            backgroundColor: "#fff",
+            borderRadius: "10px",
           }}
         >
           <View
             style={{
+              flexDirection: "row",
+              alignItems: "center",
               justifyContent: "center",
-              width: "100%",
-              alignItems: "flex-end",
             }}
           >
-            <TouchableOpacity onPress={handleComponent}>
-              <Ionicons
-                name="md-close-circle-sharp"
-                size={28}
-                color="#6a6ce0"
-              />
-            </TouchableOpacity>
+            <Text
+              style={{
+                fontWeight: 600,
+                fontSize: 22,
+                marginRight: 10,
+              }}
+            >
+              Movement Controller
+            </Text>
+            <View>
+              <TouchableOpacity
+                style={{
+                  position: "relative",
+                  width: 80,
+                  height: 40,
+                  backgroundColor: isCheckMotorMovement ? "#05c46b" : "#ddd",
+                  borderRadius: 20,
+                  justifyContent: "center",
+                  alignItems: isCheckMotorMovement ? "flex-end" : "flex-start",
+                  padding: 5,
+                  margin: 10,
+                }}
+                onPress={handleToggle}
+              >
+                <View
+                  style={{
+                    width: 30,
+                    height: 30,
+                    borderRadius: 15,
+                    backgroundColor: "#fff",
+                    shadowColor: "#000",
+                    shadowOffset: { width: 0, height: 2 },
+                    shadowOpacity: 0.2,
+                    shadowRadius: 2,
+                  }}
+                ></View>
+                <Text
+                  style={{
+                    position: "absolute",
+                    top: -25,
+                    right: -35,
+                    fontSize: 12,
+                    fontWeight: "bold",
+                    color: isCheckMotorMovement ? "#05c46b" : "#aaa",
+                    textShadowColor: "#fff",
+                    textShadowOffset: { width: 1, height: 1 },
+                    textShadowRadius: 1,
+                  }}
+                ></Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          <Text
+          {isCheckMotorMovement && (
+            <View
+              style={{ margin: 25, alignItems: "center", justifyContent: "space-evenly" }}
+            >
+              <TouchableOpacity onPressIn={MotorMovementUpIn} onPressOut={MotorMovementUpOut}>
+                <AntDesign name="caretup" size={70} color="black" />
+              </TouchableOpacity>
+              <View
+                style={{
+                  flexDirection: "row",
+                }}
+              >
+                <TouchableOpacity onPressIn={MotorMovementLeftIn} onPressOut={MotorMovementLeftOut}>
+                  <AntDesign name="caretleft" size={70} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity onPressIn={MotorMovementRightIn} onPressOut={MotorMovementRightOut} style={{ marginLeft: 30, }}>
+                  <AntDesign name="caretright" size={70} color="black" />
+                </TouchableOpacity>
+              </View>
+              <TouchableOpacity onPressIn={MotorMovementDownIn} onPressOut={MotorMovementDownOut}>
+                <AntDesign name="caretdown" size={70} color="black" />
+              </TouchableOpacity>
+            </View>
+          )}
+        </View>
+        {showComponent && (
+          <SlideUpView
+            animation="slideInUp"
             style={{
-              marginTop: 30,
-              textAlign: "left",
-              fontSize: 30,
-              fontWeight: 600,
+              backgroundColor: "#fff",
+              height: "100%",
+              width: 410,
+              position: "absolute",
+              marginLeft: -50,
+              marginTop: 100,
+              padding: 30,
+              borderRadius: 20,
             }}
           >
-            Planta De Aguacate
-          </Text>
-          <View style={{ gap: 20, marginTop: 30 }}>
-            <Text style={{ fontSize: 20 }}>Humedad: 20</Text>
-            <Text style={{ fontSize: 20 }}>Temperatura: 20</Text>
-            <Text style={{ fontSize: 20 }}>Luz: 20</Text>
-          </View>
-        </SlideUpView>
-      )}
-    </View>
+            <View
+              style={{
+                justifyContent: "center",
+                width: "100%",
+                alignItems: "flex-end",
+              }}
+            >
+              <TouchableOpacity onPress={handleComponent}>
+                <Ionicons
+                  name="md-close-circle-sharp"
+                  size={28}
+                  color="#6a6ce0"
+                />
+              </TouchableOpacity>
+            </View>
+            <Text
+              style={{
+                marginTop: 30,
+                textAlign: "left",
+                fontSize: 30,
+                fontWeight: 600,
+              }}
+            >
+              Planta De Aguacate
+            </Text>
+            <View style={{ gap: 20, marginTop: 30 }}>
+              <Text style={{ fontSize: 20 }}>Humedad: 20</Text>
+              <Text style={{ fontSize: 20 }}>Temperatura: 20</Text>
+              <Text style={{ fontSize: 20 }}>Luz: 20</Text>
+            </View>
+          </SlideUpView>
+        )}
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: 30,
+    width: "100%",
+  },
   boxShadow: {
     borderRadius: 10,
   },
@@ -160,14 +327,6 @@ const styles = StyleSheet.create({
     width: 300,
     height: 300,
     borderRadius: 9,
-  },
-  container: {
-    padding: 20,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    gap: 30,
-    width: "100%",
   },
   card: {
     marginTop: 10,
@@ -210,5 +369,13 @@ const styles = StyleSheet.create({
     height: 120,
   },
 });
+
+/*
+  shadowColor: "#000",
+  shadowOffset: { width: 0, height: 2 },
+  shadowOpacity: 0.2,
+  shadowRadius: 5,
+  elevation: 2,
+*/
 
 export default MyPlant;
